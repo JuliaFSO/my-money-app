dda348469b9e6e902d7c90be23b984e3fc969385
JuliaFSO <juliafso@gmail.com>
2023-02-10 17:46:21 -0500

    Adds routes and billingCycleService

---
+++ backend/src/config/routes.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+module.exports = function(server) {
+  // API Routes
+  const router = express.Router()
+  server.use('/api', router)
+
+  // BILLING-CYCLE Routes
+  const BillingCycle = require('../api/billingCycle/billingCycleService')
+  BillingCycleService.register(router, '/billingCycles')
+}
