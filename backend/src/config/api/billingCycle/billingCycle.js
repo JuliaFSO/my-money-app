3cf93fd5ff2bd270affd4e90208cff98286bd9b4
JuliaFSO <juliafso@gmail.com>
2023-02-13 18:41:39 -0500

    Adds cors and fix routes

--- backend/src/config/api/billingCycle/billingCycle.js
+++ backend/src/config/api/billingCycle/billingCycle.js
@@ -3,20 +3,20 @@ const mongoose = restful.mongoose
 
 const creditSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  value: { type: Number, min: 0, required: true }
+  value: { type: Number, min: 0, required: [true, 'Credit value is required.'] }
 })
 
 const debtSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  value: { type: Number, min: 0, required: true },
+  value: { type: Number, min: 0, required: [true, 'Debt value is required.'] },
   status: { type: String, required: false, uppercase: true,
     enum: ['PAID', 'PENDING', 'SCHEDULED'] }
 })
 
 const billingCycleSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  month: { type: Number, min: 1, max: 12, required: true },
-  year: { type: Number, min: 1970, max: 2100, required: true },
+  month: { type: Number, min: 1, max: 12, required: [true, 'Month is required.'] },
+  year: { type: Number, min: 1970, max: 2100, required: [true, 'Year is required.'] },
   credits: [creditSchema],
   debts: [debtSchema]
 })
