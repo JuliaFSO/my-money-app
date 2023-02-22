7b3117db81ad7f41157174251973ec4151a3998f
JuliaFSO <juliafso@gmail.com>
2023-02-22 10:23:10 -0500

    Adds footer

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -3,11 +3,16 @@ import '../common/template/dependencies';
 
 import Header from '../common/template/header';
 import Sidebar from '../common/template/sidebar';
+import Footer from '../common/template/footer';
 
 const App = props => (
   <div className='wrapper'>
     <Header />
     <Sidebar />
+    <div className='content-wrapper'>
+      <h1>Content</h1>
+    </div>
+    <Footer />
   </div>
 );
 
