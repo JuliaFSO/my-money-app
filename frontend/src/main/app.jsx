a06d52b031820c0376d664c41691276a49d42ca6
JuliaFSO <juliafso@gmail.com>
2023-02-22 17:02:48 -0500

    Adds dashboard, billingCycle and routes

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
+
 import '../common/template/dependencies';
 
 import Header from '../common/template/header';
 import Sidebar from '../common/template/sidebar';
 import Footer from '../common/template/footer';
+import Routes from './routes';
 
 const App = props => (
-  <div className='wrapper'>
-    <Header />
-    <Sidebar />
-    <div className='content-wrapper'>
-      <h1>Content</h1>
+    <div className='wrapper'>
+      <Header />
+      <Sidebar />
+      <div className='content-wrapper'>
+        <Routes />
+      </div>
+      <Footer />
     </div>
-    <Footer />
-  </div>
 );
 
 export default App;
