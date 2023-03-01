48ef2cd1ee6053c49ad3a5bdb09b712f0346be6f
JuliaFSO <juliafso@gmail.com>
2023-03-01 11:32:59 -0500

    Adds tabHeader component

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -5,6 +5,7 @@ import Content from "../common/template/content";
 import Tabs from "../common/tab/tabs";
 import TabsHeader from "../common/tab/tabsHeader";
 import TabsContent from "../common/tab/tabsContent";
+import TabHeader from "../common/tab/tabHeader";
 
 class billingCycle extends Component {
   render() {
@@ -14,7 +15,10 @@ class billingCycle extends Component {
         <Content>
           <Tabs>
             <TabsHeader>
-
+              <TabHeader label='List' icon='bars' target='tabList' />
+              <TabHeader label='Add' icon='plus' target='tabCreate' />
+              <TabHeader label='Edit' icon='pencil' target='tabUpdate' />
+              <TabHeader label='Delete' icon='trash-o' target='tabDelete' />
             </TabsHeader>
             <TabsContent>
 
