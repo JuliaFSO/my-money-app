62d8616705ce5a0bdbde53a8e27583f14ad6e496
JuliaFSO <juliafso@gmail.com>
2023-03-15 18:44:23 -0400

    Adds creditList component

--- frontend/src/billingCycle/billingCycleForm.jsx
+++ frontend/src/billingCycle/billingCycleForm.jsx
@@ -3,8 +3,9 @@ import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import { reduxForm, Field } from "redux-form";
 
-import LabelAndInput from "../common/form/labelAndInput";
 import { init } from "./billingCycleActions";
+import LabelAndInput from "../common/form/labelAndInput";
+import CreditList from "./creditList";
 
 
 class BillingCycleForm extends Component {
@@ -19,6 +20,7 @@ class BillingCycleForm extends Component {
               label="Month" cols="12 4" placeholder="MM" />
           <Field name="year" component={LabelAndInput} type="number" readOnly={readOnly}
           label="Year" cols="12 4" placeholder="YYYY" />
+          <CreditList cols="12 6" readOnly={readOnly} />
         </div>
         <div className="box-footer">
           <button type="submit" className={`btn btn-${this.props.submitClass}`}>{this.props.submitLabel}</button>
